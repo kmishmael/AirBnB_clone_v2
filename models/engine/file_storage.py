f13f5e95a6241be159d5a04bed06ebbd1792e6ab
kmishmael <kmishmael@gmail.com>
2023-09-20 18:46:58 +0300

    feat: file object deletion

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return FileStorage.__objects
+        new_dict = {}
+        for key, value in self.__objects.items():
+            if value.__class__ == cls or value.__class__.__name__ == cls:
+                new_dict[key] = value
+        return new_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +30,13 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        if obj is None:
+            return
+        key = f'{obj.__class__.__name__}.{obj.id}'
+        if key in self.__objects:
+            del self.__objects[key]
 
     def reload(self):
         """Loads storage dictionary from file"""
