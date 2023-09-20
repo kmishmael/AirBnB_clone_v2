e0864e20cd36087710bd7fa025d4e85189e0a549
kmishmael <kmishmael@gmail.com>
2023-09-20 21:08:19 +0300

    update sql previleges

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,15 @@
 -- Prepare a MySQL server
-
+-- create or replace database
 CREATE DATABASE IF NOT EXISTS hbnnb_dev_db;
+
+-- create user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+
+-- grant previleges
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- grant previlege
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- to apply changes
 FLUSH PRIVILEGES;
