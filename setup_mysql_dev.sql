d82fb86902831877b9d67cfd099813fe9b9e2fd4
kmishmael <kmishmael@gmail.com>
2023-09-21 02:33:02 +0300

    fix: database name typo

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,6 @@
 -- Prepare a MySQL server
 -- create or replace database
-CREATE DATABASE IF NOT EXISTS hbnnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- create user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
